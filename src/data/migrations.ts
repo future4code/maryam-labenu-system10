818255bb0fad4b1a69a51ada8b9bf9398020cea9
LeonardoGomesX <leo.sgomes60@gmail.com>
2021-12-08 16:10:04 -0300

    atualizando infos no banco

--- src/data/migrations.ts
+++ src/data/migrations.ts
@@ -8,12 +8,52 @@ const createTables = () =>
     connection
         .raw(
             `
-        CREATE TABLE IF NOT EXISTS aula51_users (
+        CREATE TABLE IF NOT EXISTS Classes (
             id VARCHAR(255) PRIMARY KEY,
             name VARCHAR(255) NOT NULL,
-            nickname VARCHAR(255) NOT NULL,
-            email VARCHAR(255) UNIQUE NOT NULL,
-            address VARCHAR(255) NOT NULL
+            module VARCHAR(255) DEFAULT 0
+        );
+
+
+
+        CREATE TABLE IF NOT EXISTS Students (
+            id VARCHAR(255) PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL UNIQUE,
+            birth_date DATE NOT NULL,
+            classes_id VARCHAR(255) FOREIGN KEY NOT NULL
+        );
+
+        CREATE TABLE IF NOT EXISTS Hobby_Student (
+            id VARCHAR(255) PRIMARY KEY,
+            students_id VARCHAR(255) FOREIGN KEY NOT NULL
+            hobby_id VARCHAR(255) FOREIGN KEY NOT NULL
+        );
+        
+        CREATE TABLE IF NOT EXISTS Hobby (
+            id VARCHAR(255) PRIMARY KEY,
+            name VARCHAR(255) NOT NULL UNIQUE,
+        );
+
+
+
+        CREATE TABLE IF NOT EXISTS Teachers (
+            id VARCHAR(255) PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL UNIQUE,
+            birth_date DATE NOT NULL,
+            classes_id VARCHAR(255) FOREIGN KEY NOT NULL
+        );
+        
+        CREATE TABLE IF NOT EXISTS Specialties_Teacher (
+            id VARCHAR(255) PRIMARY KEY,
+            teachers_id VARCHAR(255) FOREIGN KEY NOT NULL
+            specialties_id VARCHAR(255) FOREIGN KEY NOT NULL
+        );
+        
+        CREATE TABLE IF NOT EXISTS Specialties (
+            id VARCHAR(255) PRIMARY KEY,
+            name VARCHAR(255) NOT NULL UNIQUE,
         );
 
         `)
