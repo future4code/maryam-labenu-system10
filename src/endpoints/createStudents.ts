ce2d68888ad4ae0af9bca3a6f40d2dd656958aa1
Everton Guedes <evertonguedes1@gmail.com>
2021-12-10 15:35:57 -0300

    desenvolvimento com mestre Yuzo

---
+++ src/endpoints/createStudents.ts
@@ -0,0 +1,56 @@
+import { Request, Response } from "express";
+import { connection } from "../data/connection";
+import { Students } from "../types";
+
+export const createStudents = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { name, email, birthdate, classId, hobby } = req.body;
+
+    if (!name || !email || !birthdate || !classId || !hobby) {
+      throw new Error("Preencha todos os parâmetros");
+    }
+
+    const generateId = () => {
+      return Date.now().toString();
+    };
+
+    const student = new Students(
+      generateId(),
+      name,
+      email,
+      birthdate,
+      classId,
+      hobby
+    );
+
+    await connection("LabenuSystem_Students").insert({
+      id: student.getId(),
+      name: student.getName(),
+      email: student.getEmail(),
+      birthdate: student.getBirthdate(),
+      classId: student.getClassId(),
+    });
+
+    const hobbies = student.getHobby();
+
+    for (let hobby of hobbies) {
+      const hobbyId = generateId();
+      await connection("LabenuSystem_Hobby").insert({ id: hobbyId, name });
+
+      await connection("LabenuSystem_Hobby_Student").insert({
+        id: generateId(),
+        studentsId: student.getId(),
+        hobbyId,
+      });
+    }
+
+    res.status(201).send({ message: "Estudante criadx com sucesso!" });
+  } catch (error: any) {
+    res.status(400).send({ message: error.message });
+  }
+};
+
+export default createStudents;
