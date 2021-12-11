cd141b9b503de81d7e5f0cf5d2718a8d127d682d
Everton Guedes <evertonguedes1@gmail.com>
2021-12-10 21:22:26 -0300

    atualização branch students

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,19 @@
-import app from "./app"
-
+import app from "./app";
+import createStudents from "./endpoints/createStudents";
 import { createClasses } from "./endpoints/createClasses";
 import { getAllClasses } from "./endpoints/getAllClasses";
 import { putClassesModule } from "./endpoints/putClassModule";
-import { createTeachers} from "./endpoints/createTeacher"
-// Turma 
+import { createTeachers } from "./endpoints/createTeacher";
+// Turma
 
-app.post("/classes", createClasses)
-app.get("/classes", getAllClasses)
-app.put("/classes", putClassesModule )
-
-
-app.post("/teachers",createTeachers)
-
+app.post("/classes", createClasses);
+app.get("/classes", getAllClasses);
+app.put("/classes", putClassesModule);
 
 // Estudantes
 
-
+app.post("/students", createStudents);
 
 // Docentes
+
+app.post("/teachers", createTeachers);
